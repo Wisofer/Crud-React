380e1e78d05283a897c0aa58153bae54592b9a2d
@wisofer 17 <87929107+Wisofer@users.noreply.github.com>
2024-01-03 06:45:23 -0600

    Cambiando el Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <nav className="bg-gray-800 p-4">
@@ -11,28 +17,56 @@ const Navbar = () => {
               <span className="cursor-pointer hover:text-gray-300">Home</span>
             </Link>
           </div>
-          <div className="flex space-x-4">
-          <Link to={"formulario"}>
-              <span className="cursor-pointer text-white hover:text-gray-300">
-                Formulario
-              </span>
+          <div className="lg:hidden">
+            <button
+              className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-300 hover:text-white hover:border-white"
+              onClick={toggleMenu}
+            >
+              <svg
+                className="h-4 w-4 fill-current"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path
+                  fillRule="evenodd"
+                  d="M3 5h14a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2zm0 5h14a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2zm0 5h14a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            className={`${
+              isOpen ? "block" : "hidden"
+            } lg:flex space-x-4 lg:space-x-8`}
+          >
+            <Link
+              to={"formulario"}
+              onClick={toggleMenu}
+              className="block lg:inline-block text-white hover:text-gray-300"
+            >
+              Formulario
             </Link>
-            <Link to={"contacto"}>
-              <span className="cursor-pointer text-white hover:text-gray-300">
-                Contacto
-              </span>
+            <Link
+              to={"contacto"}
+              onClick={toggleMenu}
+              className="block lg:inline-block text-white hover:text-gray-300"
+            >
+              Contacto
             </Link>
-
-            <Link to={"nosotros"}>
-              <span className="cursor-pointer text-white hover:text-gray-300">
-                Nosotros
-              </span>
+            <Link
+              to={"nosotros"}
+              onClick={toggleMenu}
+              className="block lg:inline-block text-white hover:text-gray-300"
+            >
+              Nosotros
             </Link>
-
-            <Link to={"datos"}>
-              <span className="cursor-pointer text-white hover:text-gray-300">
-                Datos
-              </span>
+            <Link
+              to={"datos"}
+              onClick={toggleMenu}
+              className="block lg:inline-block text-white hover:text-gray-300"
+            >
+              Datos
             </Link>
           </div>
         </div>
